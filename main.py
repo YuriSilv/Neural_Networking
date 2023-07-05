0e3b27f10606fd9f6b261c65876e6700792ae5ea
YuriSilv <yuridtna2002@hotmail.com>
2023-07-04 21:30:22 -0300

    fix predict

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
      model.fit()
      print(model.predict(estimulo))"""
 
-     X_atributos = [[1, 1, 0, 0, 1, 1, 0, 0, 0], 
+     """X_atributos = [[1, 1, 0, 0, 1, 1, 0, 0, 0], 
                     [0, 0, 0, 1, 0, 1, 1, 1, 1],
                     [0, 0, 0, 0, 0, 0, 1, 1, 1],
                     [0, 0, 0, 1, 1, 1, 1, 0 ,1],
@@ -39,10 +39,28 @@ if __name__ == "__main__":
            [0, 0, 0],
            [0, 0, 0]]
      
-     n = 0.01
+     n = 0.1
 
-     model = Adaline_logistica(n, X_atributos, T_categorias, 30000, 6, 3)
+     model = Adaline_logistica(n, X_atributos, T_categorias, 3000, 6, 3)
 
      model.fit()
      
-     linear_algebra.print_matrix(linear_algebra.transposta(model.organismo))
+     linear_algebra.print_matrix(linear_algebra.transposta(model.organismo))"""
+
+     #rede "OU"
+     X_atributos = [[1, 1, -1, -1], 
+                    [1, -1, 1, -1]]
+
+     T_categorias =  [[1, 1, 1, -1]]
+     
+     w0 = [[0, 0]]
+     
+     n = 0.875
+
+     model = Adaline_logistica(n, X_atributos, T_categorias, 200, 2, 1)
+
+     model.fit()
+     
+     #linear_algebra.print_matrix(linear_algebra.transposta(model.w0))
+
+     print(model.predict(([[1],[1]])))
